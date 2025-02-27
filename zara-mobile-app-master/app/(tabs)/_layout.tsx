77edfeb9e9e656b83e3eaf74b6abe03b7faadac8
shameen <shameendilusha5678@gmail.com>
2025-02-27 14:13:37 +0530

    update _layout.tsx

---
+++ zara-mobile-app-master/app/(tabs)/_layout.tsx
@@ -0,0 +1,66 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { useColorScheme } from '@/hooks/useColorScheme.web';
+
+export default function TabLayout() {
+    const colorScheme = useColorScheme();
+
+    return (
+        <Tabs
+            screenOptions={{
+
+                headerShown: false,
+            }}>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Home',
+                    tabBarIcon: ({ color, focused }) => (
+                        <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="search"
+                options={{
+                    title: 'Search',
+                    tabBarIcon: ({ color, focused }) => (
+                        <TabBarIcon name={focused ? 'search' : 'search-outline'} color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="menu"
+                options={{
+                    title: 'Library',
+                    tabBarIcon: ({ color, focused }) => (
+                        <TabBarIcon name={focused ? 'menu' : 'menu-outline'} color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="explore"
+                options={{
+                    title: 'Explore',
+                    tabBarIcon: ({ color, focused }) => (
+                        <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+                    ),
+                }}
+            />
+
+            <Tabs.Screen
+                name="setting"
+                options={{
+                    title: 'Profile',
+                    tabBarIcon: ({ color, focused }) => (
+                        <TabBarIcon name={focused ? 'settings' : 'settings-outline'} color={color} />
+                    ),
+                }}
+            />
+
+
+        </Tabs>
+    );
+}
